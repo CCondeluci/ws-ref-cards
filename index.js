ba1ac69d7fb745e6a0d140b6048d4d38e6989519
Carmen Condeluci <crc73@pitt.edu>
2019-12-10 14:55:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
     };
 
     // get translation ref cards
-    let refcardHTML = '<html><head></head><style>div { page-break-inside:auto } table { page-break-inside:avoid; page-break-after:auto }</style><body>';
+    let refcardHTML = '<html><head></head><style>div { page-break-inside:auto } span { page-break-inside:avoid; page-break-after:auto }</style><body><div>';
+    let count = 0;
     for (let card of parsedDeck) {
         var options = {
             url: HOTC + card.ws_code + SHORT,
@@ -47,12 +48,21 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
         // set the img link to true hotc
         root('img').attr('src', HOTC_IMG + card.ws_code.replace('/', '-').toLowerCase() + '.gif');
         // rip the card translation 
-        let refCard = '<table width="400" style="border:1px solid black">' + root('table').html() + '</table>';
+        let refCard = '';
+        if (count%2 == 0 ) {
+            refCard =   '<span style="display: flex">' + 
+                        '<table width="400" style="border:1px solid black">' + root('table').html() + '</table>';
+        } else {
+            refCard =   '<table width="400" style="border:1px solid black">' + root('table').html() + '</table>' +
+                        '</span>'
+        }
         refcardHTML += refCard;
+
         console.log("DONE: " + card.ws_code);
+        count++;
     }
 
-    refcardHTML += '</body></html>';
+    refcardHTML += '</div></body></html>';
 
     fs.writeFileSync('refcards.html', refcardHTML);
 })();
